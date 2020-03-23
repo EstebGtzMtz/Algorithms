0a714c9665b22526f749c3473181243020fae43a
Esteban <esteb.gtz.mtz@hotmail.com>
2020-03-23 13:30:57 -0600

    add firstRecurringLetter algorithm
    Signed-off-by: Esteban <esteb.gtz.mtz@hotmail.com>

--- findLargestNumber.js
+++ findLargestNumber.js
@@ -1,13 +1,15 @@
-  const itemsArray = [54, 89, 45, 26, 10, 458, 639, 1002];
+const itemsArray = [54, 89, 45, 26, 10, 458, 639, 1002];
 
-  const getLargestNumber = items => {
-      let largest = -Number.MAX_VALUE;
-      items.forEach(item => {
-          if (item > largest) {
-              largest = item;
-          }
-      });
-      return console.log(largest);
-  }
+// Define a functions that find the largest number in array of numbers
 
-  getLargestNumber(itemsArray);
+const getLargestNumber = items => {
+    let largest = -Number.MAX_VALUE;
+    items.forEach(item => {
+        if (item > largest) {
+            largest = item;
+        }
+    });
+    return console.log(largest);
+}
+
+getLargestNumber(itemsArray);
---
+++ firstRecurring.js
@@ -0,0 +1,34 @@
+// Google Excersice Interview:
+// Create a function that find the firstRecurring character with given string
+
+const firstRecurring = givenString => {
+
+    //If the given string is empty, return  null
+    if (givenString === '') {
+        return console.log(null);
+    }
+
+    stringToArray = givenString.split(''); // <= Convert the string into an array to work with it
+    const counter = {}; // <= Create a hash table to save the letters
+
+    // If the array length is 1, return the letter perse
+    if (stringToArray.length === 1) {
+        return console.log(`there is just one letter: ${stringToArray[0]}`)
+    }
+
+    // Here comes the magic
+    // First we go through the array 
+    for (let i = 0; i <= stringToArray.length - 1; i++) {
+        if (stringToArray[i] in counter) {
+            // if the letter at [i] exist or in this case repeat, return the letter[i]
+            // We just found our first recurring letter 
+            return console.log(`The first recurring character is: ${stringToArray[i]}`);
+        }
+        // If not exist add it into oir Hash Table
+        counter[stringToArray[i]] = 1;
+
+    }
+}
+
+// The time complexity is O(n) == Linear time
+firstRecurring('FCAJHLKOPIJA');
