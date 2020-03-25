d033d0f7b5c35d58e8804125e4d0e59126af0380
Esteban <esteb.gtz.mtz@hotmail.com>
2020-03-25 17:28:47 -0600

    add binarySearch algorithm
    Signed-off-by: Esteban <esteb.gtz.mtz@hotmail.com>

---
+++ binarySearch.js
@@ -0,0 +1,33 @@
+const binarySearch = (target, nums) => {
+
+    let floorIndex = -1;
+    let ceilingIndex = nums.length;
+
+    while (floorIndex + 1 < ceilingIndex) {
+        const distance = ceilingIndex - floorIndex;
+        const halfDistance = Math.floor(distance / 2);
+        const guessIndex = floorIndex + halfDistance
+
+        const guessValue = nums[guessIndex];
+
+        if (guessValue === target) {
+            return true;
+        }
+
+        if (guessValue > target) {
+
+            // Target is to the left, so move ceiling to the left
+            ceilingIndex = guessIndex;
+        } else {
+
+            // Target is to the right, so move floor to the right
+            floorIndex = guessIndex;
+        }
+    }
+
+    return false;
+}
+
+const numbersArray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 30, 50];
+
+console.log(binarySearch(3, numbersArray));
